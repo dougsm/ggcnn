092db04b033b2beb25235b1155c6e996520c753a
Doug Morrison <doug.morrison91@gmail.com>
2019-03-19 07:20:40 +0100

    Download evaluation data file by default.

--- data/download_pretrained_ggcnn.sh
+++ data/download_pretrained_ggcnn.sh
@@ -3,3 +3,7 @@
 wget http://s.dougsm.com/ggcnn_rss/ggcnn_rss.tar.gz -P networks/
 tar -xvzf networks/ggcnn_rss.tar.gz -C networks/
 rm networks/ggcnn_rss.tar.gz
+
+wget http://s.dougsm.com/ggcnn_rss/val_input.zip -P networks/ggcnn_rss/
+unzip networks/ggcnn_rss/val_input.zip
+rm networks/ggcnn_rss/val_input.zip
