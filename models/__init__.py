1544a56a11d0615855a672b291684fbf8d97b987
Doug Morrison <doug.morrison91@gmail.com>
2019-07-10 14:37:19 +1000

    Added ResNet-7 based model, inspired by TossingBot

--- models/__init__.py
+++ models/__init__.py
@@ -6,5 +6,8 @@ def get_network(network_name):
     elif network_name == 'ggcnn2':
         from .ggcnn2 import GGCNN2
         return GGCNN2
+    elif network_name == 'resnet7':
+        from .ggcnn_resnet import ResNet
+        return ResNet
     else:
         raise NotImplementedError('Network {} is not implemented'.format(network_name))
