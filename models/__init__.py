ad48bc5f768fe0a9ba9fd47729638e0aed46e47b
Doug Morrison <doug.morrison91@gmail.com>
2019-08-05 15:01:04 +1000

    undo

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,5 @@ def get_network(network_name):
     elif network_name == 'ggcnn2':
         from .ggcnn2 import GGCNN2
         return GGCNN2
-    elif network_name == 'resnet7':
-        from .ggcnn_resnet import ResNet
-        return ResNet
     else:
         raise NotImplementedError('Network {} is not implemented'.format(network_name))
