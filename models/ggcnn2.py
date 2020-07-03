8104fd4426acc507359658e99dc8ec1f66d1dec6
Doug Morrison <doug.morrison91@gmail.com>
2020-07-03 12:34:14 +1000

    Use upsampling instead of inverse convolutions

--- models/ggcnn2.py
+++ models/ggcnn2.py
@@ -37,11 +37,12 @@ class GGCNN2(nn.Module):
             nn.ReLU(inplace=True),
 
             # Output layers
-            nn.ConvTranspose2d(filter_sizes[2], filter_sizes[3], 3, stride=2, padding=1, output_padding=1),
+            nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv2d(filter_sizes[2], filter_sizes[3], 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(filter_sizes[3], filter_sizes[3], 3, stride=2, padding=1, output_padding=1),
+            nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv2d(filter_sizes[3], filter_sizes[3], 3, padding=1),
             nn.ReLU(inplace=True),
-
         )
 
         self.pos_output = nn.Conv2d(filter_sizes[3], 1, kernel_size=1)
